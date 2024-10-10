a68768c11752c358bc83e82831bafbcdbd276a11
Valeria Becker <valeria.mincyt@gmail.com>
2024-10-10 13:35:49 -0300

    Se cambia parametro para generar el modelo en 002b_language_model.py

--- V2/002_Model/002b_language_model.py
+++ V2/002_Model/002b_language_model.py
@@ -50,8 +50,8 @@ print(len(affiliation_vocab))
 # %%
 # Loading the standard DistilBERT tokenizer
 #tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased", return_tensors='tf')
-
-tokenizer = DistilBertTokenizer.from_pretrained(rutaDatos, return_tensors='tf')
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+#tokenizer = DistilBertTokenizer.from_pretrained(rutaDatos, return_tensors='tf')
 
 # %%
 # Using the HuggingFace library to load the dataset
@@ -119,7 +119,8 @@ with strategy.scope():
     
     # Loading the DistilBERT model and weights with a classification head
     #model = TFAutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=len(affiliation_vocab))
-    model = TFAutoModelForSequenceClassification.from_pretrained(rutaDatos, num_labels=len(affiliation_vocab))
+    model = TFAutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased")
+    #model = TFAutoModelForSequenceClassification.from_pretrained(rutaDatos, num_labels=len(affiliation_vocab))
     
     model.compile(optimizer=opt)
 
