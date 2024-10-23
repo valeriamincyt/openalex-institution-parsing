f2ce59aa03adb4b2e63d969ccdd9c8a01d795bc2
Valeria Becker <valeria.mincyt@gmail.com>
2024-10-23 11:00:37 -0300

    Se agrega parametro para forzar download

--- V2/002_Model/modelo.py
+++ V2/002_Model/modelo.py
@@ -8,7 +8,7 @@ os.system(f"mkdir -p ./cache_TF/")
 print("Done")
 
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert/distilbert-base-uncased')
-model = TFDistilBertModel.from_pretrained("distilbert/distilbert-base-uncased")
+model = TFDistilBertModel.from_pretrained("distilbert/distilbert-base-uncased", force_download='True')
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='tf')
 output = model(encoded_input)
