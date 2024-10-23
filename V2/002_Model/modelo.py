6d0f65b83fdb6bc4a595b7dac494ebf761d35818
Valeria Becker <valeria.mincyt@gmail.com>
2024-10-23 10:55:45 -0300

    Se modifica nombre del modelo

--- V2/002_Model/modelo.py
+++ V2/002_Model/modelo.py
@@ -7,8 +7,8 @@ os.system(f"mkdir -p ./cache_TF/")
 os.system(f"mkdir -p ./cache_TF/")
 print("Done")
 
-tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-model = TFDistilBertModel.from_pretraºined("distilbert-base-uncased")
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert/distilbert-base-uncased')
+model = TFDistilBertModel.from_pretrained("distilbert/distilbert-base-uncased")
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='tf')
 output = model(encoded_input)
